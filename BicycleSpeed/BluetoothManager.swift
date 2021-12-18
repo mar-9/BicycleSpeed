06c7a0892fe70bb4681121f3f47873a0fab57f2b
MASANORI SONE <>
2021-12-18 18:34:36 +0900

    for Xcode 13

--- BicycleSpeed/BluetoothManager.swift
+++ BicycleSpeed/BluetoothManager.swift
@@ -1,100 +1,97 @@
 //
 //  BluetoothManager.swift
-//  BicycleSpeed
+//  bicycleApp
 //
-//  Copyright (c) 2015, Ernesto García
-//  Licensed under the MIT license: http://opensource.org/licenses/MIT
+//  Created by Mar 9 on 2021/12/10.
+//  
 //
 
 import Foundation
 import CoreBluetooth
 
-
-
 protocol BluetoothManagerDelegate {
-  
-  func stateChanged(state:CBCentralManagerState)
-  func sensorDiscovered( sensor:CadenceSensor )
-  func sensorConnection( sensor:CadenceSensor, error:NSError?)
-  func sensorDisconnected( sensor:CadenceSensor, error:NSError?)
+    func stateChanged(state:CBManagerState)
+    func sensorDiscovered( sensor:CadenceSensor )
+    func sensorConnection( sensor:CadenceSensor, error:Error?)
+    func sensorDisconnected( sensor:CadenceSensor, error:Error?)
 }
 
-
-
-class BluetoothManager:NSObject {
+class BluetoothManager: NSObject, CBPeripheralDelegate  {
   
+    let bluetoothCentral:CBCentralManager
+    var bluetoothDelegate:BluetoothManagerDelegate?
+    let servicesToScan = [CBUUID(string: BTConstants.CadenceService)]
   
-
-  
-  let bluetoothCentral:CBCentralManager
-  var bluetoothDelegate:BluetoothManagerDelegate?
-  let servicesToScan = [CBUUID(string: BTConstants.CadenceService)]
-  
-  override init()
-  {
-    bluetoothCentral = CBCentralManager()
-    super.init()
-    bluetoothCentral.delegate = self
-  }
-  
-  deinit {
-      stopScan()
-  }
-  
-  func startScan() {
-    
-      bluetoothCentral.scanForPeripheralsWithServices(servicesToScan, options: nil )
-  }
-  
-  func stopScan() {
-    if bluetoothCentral.isScanning {
-      bluetoothCentral.stopScan()
+    override init() {
+        bluetoothCentral = CBCentralManager()
+        super.init()
+        bluetoothCentral.delegate = self
     }
-  }
-  
-  func connectToSensor(sensor:CadenceSensor) {
-      // just in case, disconnect pending connections first
-      disconnectSensor(sensor)
-      bluetoothCentral.connectPeripheral(sensor.peripheral, options: nil)
-  }
-  
-  func disconnectSensor(sensor:CadenceSensor) {
-      bluetoothCentral.cancelPeripheralConnection(sensor.peripheral)
-  }
-  
-  func retrieveSensorWithIdentifier( identifier:String ) -> CadenceSensor? {
-    guard let uuid  = NSUUID(UUIDString: identifier) else  {
-      return nil
+
+    deinit {
+        stopScan()
     }
-    guard let peripheral = bluetoothCentral.retrievePeripheralsWithIdentifiers([uuid]).first else {
-      return nil
+
+    func startScan() {
+//        bluetoothCentral.scanForPeripherals(withServices: nil, options: nil )
+        bluetoothCentral.scanForPeripherals(withServices: [
+            CBUUID(string: "1814"),
+            CBUUID(string: "1816")
+        ], options: nil )
     }
-    return CadenceSensor(peripheral: peripheral)
-  }
-  
+
+    func stopScan() {
+        if bluetoothCentral.isScanning {
+            bluetoothCentral.stopScan()
+        }
+    }
+
+    func connectToSensor(sensor:CadenceSensor) {
+        // just in case, disconnect pending connections first
+        disconnectSensor(sensor: sensor)
+        bluetoothCentral.connect(sensor.peripheral, options: nil)
+    }
+
+    func disconnectSensor(sensor:CadenceSensor) {
+        bluetoothCentral.cancelPeripheralConnection(sensor.peripheral)
+    }
+
+    func retrieveSensorWithIdentifier( identifier:String ) -> CadenceSensor? {
+        let uuid = UUID(uuidString: identifier)
+        let nsmArray1 : [UUID]  = [uuid!]
+        guard let peripheral = bluetoothCentral.retrievePeripherals(withIdentifiers: nsmArray1).first else {
+            return nil
+        }
+        return CadenceSensor(peripheral: peripheral)
+    }
 }
 
 extension BluetoothManager:CBCentralManagerDelegate {
   
-  func centralManagerDidUpdateState(central: CBCentralManager) {
-    bluetoothDelegate?.stateChanged(central.state)
-  }
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        bluetoothDelegate?.stateChanged(state: central.state)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("Peripeherals name:\(peripheral.name ?? "")")
+        let sensor = CadenceSensor(peripheral: peripheral)
+        bluetoothDelegate?.sensorDiscovered(sensor: sensor)
+    }
   
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("did connect")
+        bluetoothDelegate?.sensorConnection(sensor: CadenceSensor(peripheral: peripheral), error: nil)
+    }
+    
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        print("did didConnectPeripheral")
+        bluetoothDelegate?.sensorConnection(sensor: CadenceSensor(peripheral: peripheral), error: nil)
+    }
   
-  func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
-      print("Peripeherals")
-    let sensor = CadenceSensor(peripheral: peripheral)
-    bluetoothDelegate?.sensorDiscovered(sensor)
-  }
-  
-  func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
-      bluetoothDelegate?.sensorConnection(CadenceSensor(peripheral: peripheral), error: nil)
-  }
-  
-  func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
-    bluetoothDelegate?.sensorConnection(CadenceSensor(peripheral: peripheral), error: error)
-
-  }
-  
-  
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        bluetoothDelegate?.sensorConnection(
+            sensor: CadenceSensor(peripheral: peripheral),
+            error: error
+        )
+    }
 }
--- BicycleSpeed/CadenceSensor.swift
+++ BicycleSpeed/CadenceSensor.swift
@@ -1,9 +1,9 @@
 //
-//  Sensor.swift
-//  BicycleSpeed
+//  CadenceSensor.swift
+//  bicycleApp
 //
-//  Copyright (c) 2015, Ernesto García
-//  Licensed under the MIT license: http://opensource.org/licenses/MIT
+//  Created by Mar 9 on 2021/12/10.
+//  
 //
 
 import Foundation
@@ -21,134 +21,151 @@ Service Cycling Speed and Cadence. Characteristic [2A55]  // Control Point
 */
 
 public struct BTConstants {
-  static let CadenceService         = "1816"
-  static let CSCMeasurementUUID     = "2a5b"
-  static let CSCFeatureUUID         = "2a5c"
-  static let SensorLocationUUID     = "2a5d"
-  static let ControlPointUUID       = "2a55"
-  static let WheelFlagMask:UInt8    = 0b01
-  static let CrankFlagMask:UInt8    = 0b10
-  static let DefaultWheelSize:UInt32   = 2170  // In millimiters. 700x30 (by default my bike's wheels) :)
-  static let TimeScale              = 1024.0
+//    static let CadenceService         = "1818"
+    static let CadenceService         = "1816"  // Cycling Speed and Cadence
+    static let CSCMeasurementUUID     = "2a5b"
+    static let CSCFeatureUUID         = "2a5c"
+    static let SensorLocationUUID     = "2a5d"
+    static let ControlPointUUID       = "2a55"
+    static let WheelFlagMask:UInt8    = 0b01
+    static let CrankFlagMask:UInt8    = 0b10
+    static let DefaultWheelSize:UInt32   = 2170  // In millimiters. 700x30 (by default my bike's wheels) :)
+    static let TimeScale              = 1024.0
 }
 
 protocol CadenceSensorDelegate {
   
-  func errorDiscoveringSensorInformation(error:NSError)
-  func sensorReady()
-  func sensorUpdatedValues( speedInMetersPerSecond speed:Double?, cadenceInRpm cadence:Double?, distanceInMeters distance:Double? )
+    func errorDiscoveringSensorInformation(error:NSError)
+    func sensorReady()
+    func sensorUpdatedValues( speedInMetersPerSecond speed:Double?, cadenceInRpm cadence:Double?, distanceInMeters distance:Double? )
 }
 
-class CadenceSensor: NSObject {
+class CadenceSensor: NSObject, CBPeripheralDelegate {
   
-  let peripheral:CBPeripheral
-  var sensorDelegate:CadenceSensorDelegate?
-  var measurementCharasteristic:CBCharacteristic?
-  var lastMeasurement:Measurement?
-  let wheelCircunference:UInt32
-  
-  
-  init(peripheral:CBPeripheral , wheel:UInt32=BTConstants.DefaultWheelSize) {
-    self.peripheral = peripheral
-    wheelCircunference = wheel
-  }
-  
-  func start() {
-    self.peripheral.discoverServices(nil)
-    self.peripheral.delegate = self
-  }
-  
-  
-  func stop() {
-    if let measurementCharasteristic = measurementCharasteristic {
-      peripheral.setNotifyValue(false, forCharacteristic: measurementCharasteristic)
-    }
-    
-  }
-  
-  func handleValueData( data:NSData ) {
-    
-      let measurement = Measurement(data: data, wheelSize: wheelCircunference)
-      print("\(measurement)")
-    
-      let values = measurement.valuesForPreviousMeasurement(lastMeasurement)
-      lastMeasurement = measurement
-      
-      sensorDelegate?.sensorUpdatedValues(speedInMetersPerSecond: values?.speedInMetersPerSecond, cadenceInRpm: values?.cadenceinRPM, distanceInMeters: values?.distanceinMeters)
-  }
-}
+    let peripheral:CBPeripheral
+    var sensorDelegate:CadenceSensorDelegate?
+    var measurementCharasteristic:CBCharacteristic?
+    var lastMeasurement:Measurement?
+    let wheelCircunference:UInt32
 
+    init(peripheral:CBPeripheral , wheel:UInt32=BTConstants.DefaultWheelSize) {
+        self.peripheral = peripheral
+        wheelCircunference = wheel
+    }
 
+    func start() {
+        self.peripheral.discoverServices(nil)
+        self.peripheral.delegate = self
+    }
 
-extension CadenceSensor : CBPeripheralDelegate {
-  
-  
-  func peripheral(peripheral: CBPeripheral, didUpdateNotificationStateForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
-    guard error == nil else {
-      sensorDelegate?.errorDiscoveringSensorInformation(NSError(domain: CBErrorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey:NSLocalizedString("Error receiving measurements updates", comment:"")]))
-      
-      return
+    func stop() {
+        if let measurementCharasteristic = measurementCharasteristic {
+            peripheral.setNotifyValue(false, for: measurementCharasteristic)
+        }
     }
-    print("notification status changed for [\(characteristic.UUID)]...")
-  }
   
-  func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
-    
-    print("Updated [\(characteristic.UUID)]...")
-    
-    guard error == nil  , let data = characteristic.value  else {
-      
-      return
+    func handleValueData( data:NSData ) {
+        let measurement = Measurement(data: data, wheelSize: wheelCircunference)
+        print("\(measurement)")
+
+        let values = measurement.valuesForPreviousMeasurement(previousSample: lastMeasurement)
+        lastMeasurement = measurement
+
+        sensorDelegate?.sensorUpdatedValues(
+            speedInMetersPerSecond: values?.speedInMetersPerSecond,
+            cadenceInRpm: values?.cadenceinRPM,
+            distanceInMeters: values?.distanceinMeters
+        )
     }
-    
-    handleValueData(data)
-    
-  }
   
-  func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
-    guard error == nil  else {
-      sensorDelegate?.errorDiscoveringSensorInformation(error!)
-      return
-    }
-    // Find the cadence service
-    guard let cadenceService =  peripheral.services?.filter({ (service) -> Bool in
-      return service.UUID == CBUUID(string: BTConstants.CadenceService)
-    }).first else {
-      
-      sensorDelegate?.errorDiscoveringSensorInformation(NSError(domain: CBErrorDomain, code: NSNotFound, userInfo: [NSLocalizedDescriptionKey:NSLocalizedString("Cadence service not found for this peripheral", comment:"")]))
-      return
-    }
-    // Discover the cadence service characteristics
-    peripheral.discoverCharacteristics(nil, forService:cadenceService )
-    print("Cadence service discovered")
-    
-  }
-  
-  func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
-    
-    guard let characteristics = service.characteristics else {
-      sensorDelegate?.errorDiscoveringSensorInformation(NSError(domain: CBErrorDomain, code: NSNotFound, userInfo: [NSLocalizedDescriptionKey:NSLocalizedString("No characteristics found for the cadence service", comment:"")]))
-      return
-      
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didUpdateNotificationStateFor characteristic: CBCharacteristic,
+        error: Error?
+    ) {
+        guard error == nil else {
+            sensorDelegate?.errorDiscoveringSensorInformation(
+                error: NSError(
+                    domain: CBErrorDomain,
+                    code: 0,
+                    userInfo: [NSLocalizedDescriptionKey:NSLocalizedString("Error receiving measurements updates", comment:"")]
+                )
+            )
+            return
+        }
+        print("notification status changed for [\(characteristic.uuid)]...")
     }
     
-    print("Received characteristics");
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didUpdateValueFor characteristic: CBCharacteristic,
+        error: Error?) {
+        print("Updated [\(characteristic.uuid)]...")
+        guard error == nil  , let data = characteristic.value  else {
+            return
+        }
+        handleValueData(data: data as NSData)
+    }
     
-    // Enable notifications for the measurement characteristic
-    for characteristic in characteristics {
-      
-      print("Service \(service.UUID). Characteristic [\(characteristic.UUID)]")
-      
-      if characteristic.UUID == CBUUID(string: BTConstants.CSCMeasurementUUID) {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverIncludedServicesFor service: CBService, error: Error?) {
+        print("didDiscoverIncludedServicesFor")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard error == nil else {
+            sensorDelegate?.errorDiscoveringSensorInformation(error: NSError(
+                    domain: CBErrorDomain,
+                    code: NSNotFound,
+                    userInfo: [NSLocalizedDescriptionKey:NSLocalizedString("didDiscoverServices", comment:error!.localizedDescription )]
+                )
+            )
+            return
+        }
         
-        print("Found measurement characteristic. Subscribing...")
-        peripheral.setNotifyValue(true, forCharacteristic: characteristic)
-        measurementCharasteristic  = characteristic
-        
-      }
+        // Find the cadence service
+        guard let cadenceService =  peripheral.services?.filter({ (service) -> Bool in
+            return service.uuid == CBUUID(string: BTConstants.CadenceService)
+        }).first else {
+            sensorDelegate?.errorDiscoveringSensorInformation(
+                error: NSError(
+                    domain: CBErrorDomain,
+                    code: NSNotFound,
+                    userInfo: [NSLocalizedDescriptionKey:NSLocalizedString("Cadence service not found for this peripheral", comment:"")]
+                )
+            )
+            return
+        }
+        // Discover the cadence service characteristics
+        peripheral.discoverCharacteristics(nil, for:cadenceService )
+        print("Cadence service discovered")
     }
-    sensorDelegate?.sensorReady()
     
-  }
-  
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didDiscoverCharacteristicsFor service: CBService,
+        error: Error?) {
+        guard let characteristics = service.characteristics else {
+            sensorDelegate?.errorDiscoveringSensorInformation(
+                error: NSError(
+                    domain: CBErrorDomain,
+                    code: NSNotFound,
+                    userInfo: [NSLocalizedDescriptionKey:NSLocalizedString("No characteristics found for the cadence service", comment:"")]
+                )
+            )
+            return
+        }
+
+        print("Received characteristics");
+
+        // Enable notifications for the measurement characteristic
+        for characteristic in characteristics {
+            print("Service \(service.uuid). Characteristic [\(characteristic.uuid)]")
+            if characteristic.uuid == CBUUID(string: BTConstants.CSCMeasurementUUID) {
+                print("Found measurement characteristic. Subscribing...")
+                    peripheral.setNotifyValue(true, for: characteristic)
+                measurementCharasteristic  = characteristic
+            }
+        }
+        sensorDelegate?.sensorReady()
+    }
 }
